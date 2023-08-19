6cba9b9fd987ed0af0cbf9e95c9a65fc2a8df3d5
Haydar <haydaramrurevanda@mail.ugm.ac.id>
2023-08-19 23:25:50 +0700

    changed port to 8080

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,14 +4,11 @@ import { router } from "../routes/product.routes";
 
 const app = express();
 
-// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// routes
 app.use("/api/v1/products", router);
 
-// db connection then server connection
 db.then(() => {
     app.listen(8080, () => {
         console.log("Server is running on port 8080");
